60d4160b56d25042bfc007c92d8f2f7de8ee25b9
Kimtaeyi <ip9368@naver.com>
2022-11-16 23:12:47 +0900

    함수

--- Section11/app.js
+++ Section11/app.js
@@ -1,5 +1,18 @@
 let age = 32;
-let userName = 'Taeyi'
+let userName = `Tae 
+yi` 
 let hobbies = ['sports','cooking','Reading'];
+let job = { 
+    title: 'developer',
+    place : 'newyotk',
+    salary: 5000};
+
 
 alert(hobbies[0]);
+
+let adultyears
+function calculateAdultYears() {
+    adultyears = age - 18 - 5*1 ;
+}
+alert(adultyears);
+age = 45; //변수 한번 선언하면 let 다시 할 필요없음
