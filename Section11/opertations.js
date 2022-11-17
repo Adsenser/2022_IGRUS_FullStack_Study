e2928ee5076effa1cdf95e253cf30fea58a22e5b
Kimtaeyi <ip9368@naver.com>
2022-11-17 15:56:18 +0900

    수 연산, 문자열, 변수 기능
    다른 작업을 수행하는 다른 스크립트가 필요할 수 있는 보다 복잡한 웹 사이트를 구축하는 경우 여러 개의 개별 스크립트 파일을 하나의 동일한 *.html 파일에 절대적으로 포함할 수 있습니다.
    
    HTML 문서에 여러 <script src="..."> 요소를 추가하기만 하면 됩니다.
    
    링크된 스크립트는 HTML 파일에 정의한 순서대로 구문 분석되고 실행됩니다. 또한 <head> 섹션이나 <body> 섹션에서 링크를 연결했는지 여부도 중요하지 않습니다(또는 이 예제와 같이 둘 다 연결하는 경우).
    
    파일 A에서 함수나 변수를 정의하고 파일 B에서 사용할 수도 있지만, 정의한 파일이 사용하는 파일보다 먼저 포함(링크)되어 있는지 확인해야 합니다.

---
+++ Section11/opertations.js
@@ -0,0 +1,11 @@
+let result
+result = result +1
+result ++ //+1 
+result += 5
+
+let userName = 'TY'
+console.log(userName.length)
+console.log(userName.toLowerCase())
+
+let hobbies = ['a','b','c']
+console.log(hobbies.length)
