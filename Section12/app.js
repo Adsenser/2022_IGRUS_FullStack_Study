d48c9f8f2d05cd82f56345a36ad4ce5c41b908ef
Kimtaeyi <ip9368@naver.com>
2022-12-29 12:33:54 +0900

    쿼리 메서드 미니 과제

--- Section12/app.js
+++ Section12/app.js
@@ -1,4 +1,13 @@
-// console.dir(window.document)
-// document.body.children[1].children[0].href = 'https://google.com';
+console.dir(window.document)
+document.body.children[1].children[0].href = 'https://google.com';
 
-document.getElementById('id1');
+// let acnchorElement = document.getElementById('id1');
+// acnchorElement.href = 'https://google.com'
+
+// document.querySelector('#id1')
+
+answer1 = document.body.children[0];
+document.body.children[1];
+document.body
+document.getElementById('id2')
+// document.querySelector('p') = 'AAAAA'
