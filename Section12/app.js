ae774d4acb4605a3e52de09e011047068fa910b0
Kimtaeyi <ip9368@naver.com>
2022-12-29 12:21:09 +0900

    쿼리 요소와 쿼리 메서드
    일반적인 쿼리 메서드 [34일차]
    자바 스크립트를 통해 HTML 요소를 쿼리/선택할 때 일반적으로 사용되는 몇 가지 기본 제공 메서드가 있습니다.
    
    ● document.getElementById('some-id'): ID로 HTML 요소 선택(ID는 고유해야 하므로 하나의 요소만 선택)
    
    ● document.querySelector('<some-css-selector>'): 제공된 CSS 선택자에 의해 충족/선택된 첫 번째 일치(!) HTML 요소를 선택합니다. CSS 선택자는 기본적으로 모든 종류의 유효한 CSS 선택자(예: ID 선택자, 태그 유형 선택자, 클래스 선택자, 결합 선택자 등)일 수 있습니다.
    
    ● document.querySelectorAll('<some-css-selector>'): 제공된 CSS 선택자에 의해 충족/선택된 일치하는 모든 HTML 요소를 선택합니다.
    
    덜 사용되는 선택 방법도 몇 가지 있습니다.
    
    ● document.getElementsByClassName('some-css-class'): 제공된 CSS 클래스가 있는 모든 HTML 요소를 선택합니다.
    
    ● document.getElementsByTagName('tag'): 제공된 HTML 태그 유형의 모든 HTML 요소를 선택합니다.

--- Section12/app.js
+++ Section12/app.js
@@ -1,2 +1,4 @@
 // console.dir(window.document)
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
+
+document.getElementById('id1');
