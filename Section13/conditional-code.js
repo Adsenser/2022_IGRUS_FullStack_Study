8cdeb7ff49fd5493db92e8965790a57e03d4fee9
Kimtaeyi <ip9368@naver.com>
2023-01-05 21:24:24 +0900

    section 13 미니과제 if문

---
+++ Section13/conditional-code.js
@@ -0,0 +1,24 @@
+
+const productNamenInputElement = document.getElementById('product-name');
+const remainingCharsElement = document.getElementById('remaining-chars');
+
+// console.dir(productNamenInputElement);
+
+const maxAllowedChars = productNamenInputElement.maxLength; //maxlength가 아니라 maxLength 임에 주의하자 근데. html 상에서는 maxlength임
+
+function updateRemaingCharacters(event) {
+    const enteredText= event.target.value;
+    const enteredTextLength = enteredText.length; //앞에서 정의한 entredText가 문자열로 받아진다는 것을 알기때문
+
+    const remainingCharacters = maxAllowedChars - enteredTextLength;
+
+    remainingCharsElement.textContent = remainingCharacters;
+
+    if ( remainingCharacters <= 10) {
+        productNamenInputElement.className = 'input.warning';
+    }
+}
+
+
+
+productNamenInputElement.addEventListener('input',updateRemaingCharacters);
