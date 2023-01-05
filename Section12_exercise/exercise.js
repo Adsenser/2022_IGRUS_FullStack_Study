4bc1e1176ab666b0d5be55592fa226320a8412ff
Kimtaeyi <ip9368@naver.com>
2023-01-05 18:05:34 +0900

    section12 최종과제 정답과 내풀이 비교

--- Section12_exercise/exercise.js
+++ Section12_exercise/exercise.js
@@ -4,10 +4,10 @@
 //    - Select the first button without adding or using any "id"
 //    - Select the second button by using an "id"
 
-console.dir(window.document.body.children[2]);
 
-const button1 = document.body.children[2].children[4];
-const button2 = document.getElementById('button2');
+const button1 = document.querySelector('button');
+const button2 = document.querySelector('#button2'); //쿼리선택자는 css 선택자를 값으로 원함에 주의
+
 
 // 2) Add "click" event listener to both buttons (with two different functions).
 //    The functions should "console.dir()" the clicked buttons.
@@ -15,11 +15,11 @@ const button2 = document.getElementById('button2');
 //    - Output the second button WITHOUT using the variable in which it's stored
 
 function clickFunction1() {
-    console.dir(document.body.children[2].children[4])
+    console.dir(button1)
 }
 
-function clickFunction2() {
-    console.dir(document.body.children[2].children[6])
+function clickFunction2(event) {
+    console.dir(event.target) //사실 event 이부분 잘 이해를 못함;
 }
 
 button1.addEventListener('click', clickFunction1);
@@ -33,7 +33,7 @@ button2.addEventListener('click', clickFunction2);
 
 //어케 둘을 동시에 선택해서 한 변수에 넣는담...;;
 let paragraph1 = document.body.children[2].children[1];
-let paragraph2 = document.body.children[2].children[3];
+let paragraph2 = paragraph1.nextElementSibling.nextElementSibling;
 
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
@@ -43,11 +43,14 @@ function clickFunction1() {
     paragraph1.remove();
 }
 
-function clickFunction2() {
-    paragraph2.style.backgroundColor = 'blue';
-}
+// 5번 문제 실행되는지 확인하기 위해 일단 주석 처리했음
+// // function clickFunction2() {
+// //     paragraph2.style.backgroundColor = 'blue';
+// }
 
 // 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
 //    Note: You'll have to add those classes to the styles.css file first!
 
-paragraph2.classList.add('set-color');
+function clickFunction2() {
+    paragraph2.className = 'blue-bg';
+}
