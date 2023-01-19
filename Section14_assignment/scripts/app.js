f69e2ef667991d29fb7394a74d38e96fdabca0a9
Kimtaeyi <ip9368@naver.com>
2023-01-19 20:27:41 +0900

    틱텍토 - 오버레이 표시/숨기기

--- Section14_assignment/scripts/app.js
+++ Section14_assignment/scripts/app.js
@@ -3,6 +3,10 @@ const backdropElement = document.getElementById('backdrop')
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const cancelConfigButtonElement = document.getElementById('cancel-config-btn');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
+
+cancelConfigButtonElement.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
