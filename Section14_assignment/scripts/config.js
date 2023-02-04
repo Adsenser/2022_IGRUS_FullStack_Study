190c3a04bb6f9eca19c906180ad1e58600d42de0
Kimtaeyi <ip9368@naver.com>
2023-02-04 19:31:01 +0900

    nodeJS- localhost 실습

--- Section14_assignment/scripts/config.js
+++ Section14_assignment/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = event.target.dataset.playerid;
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -11,6 +12,14 @@ function closePlayerConfig() {
 function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername');
-    
+    const enteredPlayername = formData.get('playername').trim();
+
+    if(!enteredPlayername) {
+        errorsOutputElement.textContent = 'Plz enter a valid name';
+        return;
+    }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data' );
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
 }
+
