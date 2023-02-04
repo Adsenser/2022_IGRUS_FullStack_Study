53e73ae81c097cfe9c0f9ebb82c108854ab4bcac
Kimtaeyi <ip9368@naver.com>
2023-02-04 19:41:17 +0900

    if문 활용해 url입력에 따른 서버측 작업

--- Section16/app.js
+++ Section16/app.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 function handleRequest(request, response) {
-    response.statusCode = 200;
-    response.end('<h1>Hello TY</h1>');
+
+    if (request.url === '/currenttime') {
+        response.statusCode = 200;
+        response.end('<h1>' +new Date().toISOString() + '</h1>'); //toIOString을 호ㄹ해 이 날짜 객체를 문자열 표현으로 변환해서 읽을 수  있는 텍스트로 변환 가능
+    } else if (request.url === '/') {
+        response.statusCode = 200;
+        response.end('<h1>Hello TY</h1>');
+    }
+    
+ 
 }
 
 const server = http.createServer(handleRequest);
