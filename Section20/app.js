b36a456167668dea8097a88738bb53bd4b39f726
Kimtaeyi <ip9368@naver.com>
2023-02-22 20:34:44 +0900

    동적 경로

--- Section20/app.js
+++ Section20/app.js
@@ -27,10 +27,18 @@ app.get('/restaurants', function (req, res) {
   });
 });
 
-app.get('/restaurants/:id', function (req, res) {
-  const restaurantId = req.params.id;
-  res.render('restaurant-detail', { rid: restaurantId });
+//1. <- :을 이용해서 동적 id 생성 파트
+app.get('/restaurants/:inha', function (req, res) {
+  const restaurantId = req.params.inha; //요청 객체 생성, id는 서로 같아야 한다.
+  // params 자체에는 실제로 객체가 포함되어 있고 해당 객체에는 키로 정의한 동적 placeholder가 있음
+  // placeholder: 빠져있는 다른것을 대체하는 것
+  res.render('restaurant-detail', { rid: restaurantId }); //레스토랑 id를 여기 rid키에 값으로 전달할 예정
+  //그러면 해당 템플릿 내부의 변수로 저 rid 키를 사용할 수 있음 (저 id데이터를 템플릿에서 사용 가능)
 });
+// 3. http://localhost:3000/restaurants/aa <-여기서 aa를 주목
+// app.js로 작성한 코드는 여기 위의 파트(app.js)의 동적 부분을 수락한 후 저 aa가 포함된 url의 구체적인 값을 얻게됨.
+
+// 4. css 코드가 제대로 로드되지 않았던 이유: 상대경로 문제 때문. -> 존재하지 않는 /:inha에서 스타일 폴더를 찾게 되는것임. 
 
 app.get('/recommend', function (req, res) {
   res.render('recommend');
