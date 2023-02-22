de1c0c51bacc559cc78b4b08a5a926d696b5b942
Kimtaeyi <ip9368@naver.com>
2023-02-22 21:47:55 +0900

    서버측 오류 처리 (500)

--- Section20/app.js
+++ Section20/app.js
@@ -71,9 +71,12 @@ app.get('/about', function (req, res) {
   res.render('about');
 });
 
-app.use(function (req, res) { //use사용시 들어오는 모든 요청에 대해 실행하는 함수 적용 가능
+app.use(function (req, res) {
   res.render('404');
 });
-// 코드가 읽혀지는 순서가 <위에서 아래로 순차적> 1. 새로운 요청 서버에 도착 2. 미들웨어를 통해 유입 3. 처리해야되는 하나의 경로를 찾기 위해 다른 경로 확인 4. 해당 요청을 처리하는 경로 없을땐 404페이지 송출
-// 따라서 404를 처리하는 미들웨어는 맨 마지막에 와야함 
+
+app.use(function (error, req, res, next) { //얘는 4개의 매개변수를 수신해야함
+  res.render('500');
+});
+
 app.listen(3000);
