f8e37b8a78effc3ad8cf69935a4e50ea051f60ac
Kimtaeyi <ip9368@naver.com>
2023-02-22 21:21:55 +0900

    세부데이터 로드 및 표시하기

--- Section20/app.js
+++ Section20/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const uuid = require('uuid'); //'uuid'에서 객체를 얻는다는 의미임
+const uuid = require('uuid');
 
 const app = express();
 
@@ -28,18 +28,19 @@ app.get('/restaurants', function (req, res) {
   });
 });
 
-//1. <- :을 이용해서 동적 id 생성 파트
-app.get('/restaurants/:inha', function (req, res) {
-  const restaurantId = req.params.inha; //요청 객체 생성, id는 서로 같아야 한다.
-  // params 자체에는 실제로 객체가 포함되어 있고 해당 객체에는 키로 정의한 동적 placeholder가 있음
-  // placeholder: 빠져있는 다른것을 대체하는 것
-  res.render('restaurant-detail', { rid: restaurantId }); //레스토랑 id를 여기 rid키에 값으로 전달할 예정
-  //그러면 해당 템플릿 내부의 변수로 저 rid 키를 사용할 수 있음 (저 id데이터를 템플릿에서 사용 가능)
-});
-// 3. http://localhost:3000/restaurants/aa <-여기서 aa를 주목
-// app.js로 작성한 코드는 여기 위의 파트(app.js)의 동적 부분을 수락한 후 저 aa가 포함된 url의 구체적인 값을 얻게됨.
+app.get('/restaurants/:id', function (req, res) {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
-// 4. css 코드가 제대로 로드되지 않았던 이유: 상대경로 문제 때문. -> 존재하지 않는 /:inha에서 스타일 폴더를 찾게 되는것임. 
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render('restaurant-detail', { restaurant: restaurant }); //값에 해당하는 restaurant는 for 문에서 정의한 상수 restaurant임
+    }
+  }
+});
 
 app.get('/recommend', function (req, res) {
   res.render('recommend');
@@ -47,8 +48,7 @@ app.get('/recommend', function (req, res) {
 
 app.post('/recommend', function (req, res) {
   const restaurant = req.body;
-  restaurant.id = uuid.v4(); //존재하지 않는 필드에 액세스 ->js에선 js가 간단히 생성해줌 //이건 속성으로 들어오는 객체의 일부가 아님
-  // v4: 무작위로 생성되지만 고유함을 보장하는 고유한 id를 제공해 줄것임
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
